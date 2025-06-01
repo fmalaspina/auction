62b68ff1f0cf1613ac2b90b152c0f6fb176de542
fmalaspina <f_malaspina@hotmail.com>
2025-06-01 19:09:38 -0300

    se agregan comentarios

--- Auction.sol
+++ Auction.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
+/// @title Subasta pública
+/// @author Fernando Malaspina
+/// @notice Este contrato permite ofertar en subastas.
+/// Las reglas de la subasta son las siguientes:
+/// - No se admiten pujas inferiores a un 5% mayores a la ultima puja.
+/// - El precio base de la subasta es 1 ether.
+/// - La duración estipulada es de 1 hora.
+/// - El plazo de la subasta se extiende en 10 minutos con cada nueva oferta válida. 
+/// Esta regla aplica siempre a partir de 10 minutos antes del plazo original de la subasta. 
+/// De esta manera los competidores tienen suficiente tiempo para presentar una nueva oferta si así lo desean.
 contract Auction {
-
     // Comienzo de la puja
     uint256 private startDate;
     // Fin de la puja
@@ -16,6 +24,7 @@ contract Auction {
     uint256 private winnerBid;
     // Incremento requerido entre una puja y otra
     uint256 private increment;
+    // Estructura para que la devolución de funcion getAllBids sea legible
     struct Bid {
         address bidder;
         uint256 amount;
@@ -36,29 +45,34 @@ contract Auction {
         winnerBid = 1 ether;
         increment = 105;
     }
-
+    
+    /// @notice Se emite cuando se realiza una nueva puja válida.
+    /// @param bidder Dirección del usuario que hizo la puja
+    /// @param value  Valor de la puja en wei
     event newBid(address indexed bidder, uint256 value);
+    
+    /// @notice Se emite cuando la subasta ha finalizado.
+    /// @param winnerBidder Dirección del usuario que ganó la subasta
+    /// @param winnerBid  Valor de la puja en wei
+    event auctionFinnished(address indexed winnerBidder, uint256 winnerBid);
 
-    event end(address indexed bidder, uint256 value);
-
-    // Requiere que la puja esté activa
     modifier onlyStillActive() {
         require(block.timestamp <= endDate, "Contrato expirado");
         _;
     }
-    // Requiere que el ofertante sea otro distinto del ganador actual
+
     modifier onlyOtherThanWinnerBidder() {
         require(msg.sender != winnerBidder, "Usted ya tiene la mejor puja");
         _;
     }
 
-    // Requiere que la oferta supere la anterior en un incremento establecido en constante
     modifier onlyGreaterBid() {
         require(msg.value > (winnerBid * increment) / 100, "Puja con menos de 5% de incremento sobre la ganadora");
         _;
     }
 
-    // Funcion para aceptar la nueva puja
+    /// @notice Permite hacer una nueva puja si cumple las reglas de la subasta
+    /// @dev Lanza error si no supera el 5% o si ya es el ofertante ganador
     function bid() onlyStillActive() onlyGreaterBid() onlyOtherThanWinnerBidder external payable  {
         emit newBid(msg.sender, msg.value);
         winnerBidder = msg.sender;
@@ -69,8 +83,9 @@ contract Auction {
         bids[msg.sender] = msg.value;
 
     }
-
-    // Funcion para devolver todas las pujas hasta el momento
+    
+    /// @notice Devuelve todas las pujas realizadas en formato estructurado
+    /// @return Una lista de structs con el address del postor y su puja
     function getAllBids() external view returns (Bid[] memory) {
         uint256 len = bidders.length;
         Bid[] memory all = new Bid[](len);
@@ -88,9 +103,13 @@ contract Auction {
     } 
 
 
-    // Devuelve la oferta ganadora
+    /// @notice Devuelve el ganador de la subasta y su puja
+    /// @return Struct Bid con address y valor ofertado
     function getWinnerBid() external view returns (Bid memory) {
         require(block.timestamp > endDate, "Contrato no finalizado");
         return Bid({bidder: winnerBidder, amount: winnerBid});
     }
+
+
+
 }
